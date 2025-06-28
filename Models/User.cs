5e6ad9e4e0df59a0feba7c4625bfca1b53e8092c
Kreditkao <bermutok@gmail.com>
2025-06-28 11:23:48 +0300

    Create User.cs

---
+++ Models/User.cs
@@ -0,0 +1,8 @@
+using System;
+using System.Threading;
+public class User
+{
+    public int Id { get; set; }
+    public string Username { get; set; } = "";
+    public string FullName { get; set; } = "";
+}
